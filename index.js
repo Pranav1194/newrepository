e7fd5a7b8bbd8a6f7bcd5ec6a369c0ca1494e2e4
Pranav1194 <156526912+Pranav1194@users.noreply.github.com>
2024-03-29 15:12:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
  
 var app = express();//Respond with "deployeed" for requests that hit our root "/"
 app.get('/', function (req, res) {
- res.send('hello');
+ res.send('done');
 });//listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
  
